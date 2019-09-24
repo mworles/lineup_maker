b7b2abb7d581dd25ccfb243c4041c87cd8d9adda
Matthew Worley <mworles@gmail.com>
2019-09-24 12:39:47 -0700

    activated imports in update_pool

--- update_pool.py
+++ update_pool.py
@@ -5,5 +5,5 @@ url = 'https://www.numberfire.com/nfl/fantasy/fantasy-football-projections'
 urls = [url, url + '/d']
 
 scrape(urls)
-#import filter_players
-#import assign_id
+import filter_players
+import assign_id
